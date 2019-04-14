0319ba4b0890b8f926e5db9f44337a4de566fb85
Ian Atha <ian@mamabear.io>
2019-04-14 12:23:44 -0700

    code cleanup

--- lib/jsonschema.ex
+++ lib/jsonschema.ex
@@ -5,14 +5,13 @@ defmodule JSONSchema do
           required(:type) => String.t()
         }
 
-  # @spec field_typedef_simple(String.t(), String.t(), String.t()) :: schema_result
+  @spec field_typedef_simple(String.t(), String.t(), String.t(), keyword()) :: schema_result
   defp field_typedef_simple(root, name, type, meta \\ []) do
     %{
       "$id": root <> "/properties/" <> name,
       type: type
     }
-    |> Map.merge(if meta[:examples], do: %{examples: meta[:examples]}, else: %{})
-    |> Map.merge(if meta[:title], do: %{title: meta[:title]}, else: %{})
+    |> Map.merge(Enum.into(meta, %{}))
   end
 
   @spec get_meta(atom, atom) :: []
@@ -20,7 +19,7 @@ defmodule JSONSchema do
     apply(module, :__meta, [])[field_name_atom]
   end
 
-  @spec field_typedef(String.t(), String.t(), map) :: {:error, map} | schema_result
+  @spec field_typedef(String.t(), atom, map, keyword) :: {:error, map} | schema_result
   defp field_typedef(root, field_name_atom, typedef, meta \\ []) do
     field_name = field_name_atom |> Atom.to_string()
 
@@ -50,24 +49,29 @@ defmodule JSONSchema do
     end
   end
 
+  @spec field(atom, String.t(), {:type, integer, :map_field_exact, nonempty_list}) ::
+          nil | {:error, map} | {atom, map}
+
   defp field(_module, _root, {:type, _lineno, :map_field_exact, [{:atom, _, :__struct__} | _]}) do
     nil
   end
 
-  #  @spec field(String.t(), {:type, integer, :map_field_exact, nonempty_maybe_improper_list()}) ::
-  # nil | {atom(), {:error, map()} | map()}
   defp field(module, root, {:type, _lineno, :map_field_exact, [{:atom, _, field_name} | typedef]}) do
-    {field_name, field_typedef(root, field_name, typedef |> hd, get_meta(module, field_name))}
+    field_schema = field_typedef(root, field_name, typedef |> hd, get_meta(module, field_name))
+
+    case field_schema do
+      {:error, _} -> field_schema
+      _ -> {field_name, field_schema}
+    end
   end
 
-  @spec field_typedef_object(atom(), String.t()) :: schema_result
+  @spec field_typedef_object(atom, String.t()) :: schema_result
   defp field_typedef_object(module, name) do
-    import Enum, only: [map: 2, reject: 2]
-
     {:ok, [type: typedef]} = Code.Typespec.fetch_types(module)
     {:t, {:type, _lineno, :map, fields}, []} = typedef
 
-    properties = fields |> map(&field(module, name, &1)) |> reject(&is_nil/1) |> Map.new()
+    properties =
+      fields |> Enum.map(&field(module, name, &1)) |> Enum.reject(&is_nil/1) |> Map.new()
 
     %{
       "$id": name,
@@ -76,7 +80,7 @@ defmodule JSONSchema do
     }
   end
 
-  @spec schema(atom()) :: schema_result
+  @spec schema(atom) :: schema_result
   def schema(module) do
     field_typedef_object(module, "#")
   end
