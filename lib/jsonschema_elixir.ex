762c122612b640367b1800c532d51d665d4b1476
Ian Atha <ian@mamabear.io>
2019-04-13 22:27:40 -0700

    Initial commit.
    mix new jsonschema_elixir

---
+++ lib/jsonschema_elixir.ex
@@ -0,0 +1,18 @@
+defmodule JsonschemaElixir do
+  @moduledoc """
+  Documentation for JsonschemaElixir.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> JsonschemaElixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/jsonschema_elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule JsonschemaElixirTest do
+  use ExUnit.Case
+  doctest JsonschemaElixir
+
+  test "greets the world" do
+    assert JsonschemaElixir.hello() == :world
+  end
+end
